dab3428a8221e95aeac76ac883acf8104a2af70d
Starla <starla@Joes-iMac.local>
2016-07-28 21:25:27 -0700

    Finishing place-my-order

--- build.js
+++ build.js
@@ -3,5 +3,30 @@ var stealTools = require("steal-tools");
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
 }, {
-  bundleAssets: true
+  bundleAssets: {
+    infer: false,
+    glob: "node_modules/place-my-order-assets/images/**/*"
+  }
 });
+// options added by `donejs add cordova` - START
+var cordovaOptions = {
+  buildDir: "./build/cordova",
+  id: "com.donejs.placemyorder",
+  name: "place my order",
+  platforms: ["ios"],
+  plugins: ["cordova-plugin-transport-security"],
+  index: __dirname + "/production.html",
+  glob: [
+    "node_modules/steal/steal.production.js",
+    "node_modules/place-my-order-assets/images/**/*"
+  ]
+};
+
+var stealCordova = require("steal-cordova")(cordovaOptions);
+// Check if the cordova option is passed.
+var buildCordova = process.argv.indexOf("cordova") > 0;
+
+if(buildCordova) {
+  buildPromise.then(stealCordova.build).then(stealCordova.ios.emulate);
+}
+// options added by `donejs add cordova` - END
