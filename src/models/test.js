fc45e9f422aeb1766bcd657bd3c9193a10353d9c
Starla <starla@Joes-iMac.local>
2016-07-28 18:30:18 -0700

    All stuff up to Create Documentation

--- src/models/test.js
+++ src/models/test.js
@@ -5,3 +5,5 @@ import 'place-my-order/models/restaurant_test';
 import 'place-my-order/models/state_test';
 
 import 'place-my-order/models/city_test';
+
+import 'place-my-order/models/order_test';
--- src/test/test.js
+++ src/test/test.js
@@ -2,3 +2,5 @@ import 'place-my-order/models/test';
 import 'place-my-order/test/functional';
 
 import 'place-my-order/restaurant/list/list_test';
+
+import 'place-my-order/order/new/new_test';
